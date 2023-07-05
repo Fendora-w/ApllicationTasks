d611c411dbb4959a908db513784379649ba2bcd0
fedora-w <evangeli985107620@gmail.com>
2023-07-05 15:28:36 -0300

    Versão atualizada 1.0.2

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 class Agenda {
 
     constructor(){
@@ -14,25 +15,27 @@ class Agenda {
         this.li = li;
     }
     
-    //Cria tag do Icone da lixeira
-    criaIconeLixeira(){
+  //Cria tag do Icone da lixeira
+    criaIconeLixeira(id){
         const lx = document.createElement('lixeira');
         lx.className = 'lx';
         const i = document.createElement('i');
         i.className = 'fa fa-trash hide';
         i.id = 'lixeira'
+        i.setAttribute('onclick', `lixeira('${id}')`)
         lx.appendChild(i)
         this.li.appendChild(lx)  
-       
+   
     }
 
     //Cria tag do Icone do checkout
-    criaIconeCheckout() {
-       
+    criaIconeCheckout(id) {
+    
         const i = document.createElement('i');
-        i.id = 'icone'
+        i.id = 'icone';
         i.className = 'fa fa-check';
-        this.li.appendChild(i)
+        i.setAttribute('onclick', `checkout('${id}')`);
+        this.li.appendChild(i);
 
     }
     
@@ -45,9 +48,20 @@ class Agenda {
 
     }
 
+
+    //cria Icone editar Tarefa
+    criaIconeEditar(id){
+        const i = document.createElement('i')
+        i.className = "fas fa-edit"
+        i.id = `${id}`
+        this.li.appendChild(i)
+    
+    }
+
+
+
+
 }
-
-
 
 const ag = new Agenda();
 
@@ -73,32 +87,43 @@ ag.addBtn.addEventListener('click', function(e) {
     try {
         for(let i=0; i < filhos.length; i++){
             const item = filhos[i]
+            // console.log(filhos)
             const listTextSpan = item.querySelector('span')
-            if(ag.tarefa.value.toUpperCase().trim() === listTextSpan.textContent){
+            // console.log(ag.tarefa.value.toUpperCase().trim())
+            console.log(listTextSpan)
+            if(listTextSpan && ag.tarefa.value.toUpperCase().trim() === listTextSpan.innerText){
                 ag.tarefa.value = ''
                 throw new Error()
             }
             
         }
     }catch(erro){
-        console.log('Valor tarefa já existe')
+        console.log(erro)
         return
     }
    
-    
+   
     //cria elmento li
     const li = document.createElement('li')
+    //cria id de identificar da tag li
+    const timestamp = Date.now();
+    const id = `li-${timestamp}`;
+    li.setAttribute('id', id);
     ag.set(li)
-   
+
+    //cria o icone editar tarefa
+    ag.criaIconeEditar(id)
+
     //Cria o icone de checkout
-    ag.criaIconeCheckout()
+    ag.criaIconeCheckout(id)
    
-    //cria a tag
+    //cria a tag span adicionar tarefa
     ag.criaTagSpan()
     
     //cria icone de Lixeira
-    ag.criaIconeLixeira()
-
+    ag.criaIconeLixeira(id)
+    
+    
     //Limpa o campo de adicionar tarefas
     ag.tarefa.value = '';
     
@@ -111,46 +136,101 @@ ag.addBtn.addEventListener('click', function(e) {
 
 
 
-
-//Bloco apaga o itens do botão lixeira.
 //Acessa lista filhos da tag ol tarefas
 const itensLista = ag.listaOrdenada.children;
 
-ag.listaOrdenada.addEventListener('mouseover', function() {
+//Lixeira apaga a tarefa 
+function lixeira(id){
    
-    for(let i = 0; i < itensLista.length; i++){
-        // console.log(itensLista[i].id)
-        const item = itensLista[i]
-        const tagLixeira = item.querySelector('lixeira')
-        const idLixeira = item.querySelector('#lixeira')
+  const item = document.getElementById(id)
+  
+  //remove a tarefa realizada
+  if(item){
+    item.remove()
+  }
+
+  //Reabilita o radape novamente
+ if(itensLista.length <= 0){
+    ag.rodape.style.display = 'block'
+ }
+
+ 
+
+}
+
+
+//Checkout da tarefa realizada com sucesso
+function checkout(id){
+
+    const item = document.getElementById(id)
+    
+    if(item){
+         
         const icone = item.querySelector('#icone')
-        
-        //Bloco de ação click sobre icone de Checkout
-        //altera a cor do icone     
-        icone.addEventListener("click", function() {
-            icone.style.color = "#0DD657";
-            icone.style.borderColor = "#0DD657";
-            idLixeira.style.display = "inline";
-        });
-        
-        //Bloco de ação de lixeira ícone apaga tags <li> das tarefas
-        tagLixeira.addEventListener('click', function(){
+        const idLixeira = item.querySelector('#lixeira')
+        const btnEditar = item.querySelector('.fa-edit')
 
-            tagLixeira.remove()
-            item.remove()
-
-            //Reabilita o radape novamente
-            if(itensLista.length <= 0){
-                ag.rodape.style.display = 'block'
-            }
-                
-        })     
+        icone.style.color = "#0DD657";
+        icone.style.borderColor = "#0DD657";
+        idLixeira.style.display = "inline";
+        btnEditar.remove()
     }
+
+}
+
+//manipular tarefa editar
+//codigo atualizado
+ag.listaOrdenada.addEventListener('click', function(evento) {
+    const elementoClicado = evento.target;
     
-    
-})
+    if (elementoClicado.className === 'fas fa-edit' || elementoClicado.className === 'task') {
+        
+        for (let i = 0; i < itensLista.length; i++) {
+            const item = itensLista[i];
+            
+            if (elementoClicado.id === item.id) {
+
+                const span = item.querySelector('span');
+                const originalText = span.textContent
+                console.log(originalText)
+                
+                
+                const li = document.createElement('li');
+                const input = document.createElement('input');
+                const button = document.createElement('button');
+                const i = document.createElement('i');
+                
+                li.id = item.id;
+                input.id = 'task';
+                input.name = 'task';
+                input.type = 'text';
+                input.style.marginRight = '4px';
+                input.value = originalText;
+                
+                i.className = 'fa fa-plus';
+                button.type = 'submit';
+                button.id = 'add';
+                button.appendChild(i);
+                
+                button.addEventListener('click', function() {
+                    const newText = document.createTextNode(input.value.toUpperCase().trim());
+                    span.textContent = ''; // Limpa o conteúdo atual do <span>
+                    span.appendChild(newText); // Adiciona o novo valor como um nó de texto
+                    li.removeChild(input);
+                    li.removeChild(button);
+                });
+                
+                li.appendChild(input);
+                li.appendChild(button);
+                
+                item.parentNode.insertBefore(li, item.nextSibling); // Insere após o item atual
+            }
+        }
+    }
+});
 
 
+  
 
 
 
